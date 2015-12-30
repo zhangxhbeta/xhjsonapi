7a0a7bf54957ec6e5ac14b49b9a59e791d03bdec
zhangxh <zhangxhbeta@gmail.com>
2015-12-30 16:05:10 +0800

    基于 frisbee 实现 jsonrpc 功能

--- src/xhjsonapi.js
+++ src/xhjsonapi.js
@@ -1,7 +1,59 @@
-const XHJsonApi = {
-  greet() {
-    return 'hello';
-  }
-};
+/**
+生成符合 jsonrpc 2.0 规范的 api，基于 frisbee
 
-export default XHJsonApi;
+@author zhangxhbeta
+*/
+
+import Frisbee from 'frisbee';
+
+export default class XHJsonApi extends Frisbee{
+
+  constructor(opts) {
+    super(opts);
+
+    if (!this.opts.rpcPath) {
+      throw new Error('需要提供rpc路径参数（rpcPath）');
+    }
+
+    if (!this.opts.methods) {
+      throw new Error('需要提供方法定义列表（methods）');
+    }
+
+    this.opts.methods.forEach(function(namespace) {
+      this[namespace.name] = namespace.methods.reduce(function(result, method) {
+        // 依次设置方法函数
+        result[method] = function(params, callback) {
+          const options = {
+            jsonrpc: '2.0',
+            method: namespace.name + '.' + method,
+            id: id,
+            params: params,
+          };
+          return this.post(this.opts.rpcPath, options, callback)
+            .then(function(response, body) {
+              // 这里接收到 body 和 原始的 response
+              if (typeof body === 'object') {
+                const result = body.result;
+                const error = body.error;
+                if (!result) {
+                  // 成功返回结果
+                  return {result, body};
+                } else if (!error) {
+                  if (typeof error === 'object' && error.code && error.message) {
+                    throw new Error({code: error.code, message: error.message});
+                  } else {
+                    throw new Error({code: -32700, message: '无法解析响应的错误信息'});
+                  }
+                } else {
+                  throw new Error({code: -32700, message: '无法解析响应'});
+                }
+              } else {
+                throw new Error({code: -32700, message: '无法解析响应'});
+              }
+            });
+        };
+        return result;
+      }, {});
+    });
+  }
+}
